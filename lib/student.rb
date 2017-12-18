52b669b0e0c05d2282375402c92b085374fe2a2f
Chris Harvey <chharvey@icloud.com>
2017-12-18 05:16:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,4 +50,20 @@ class Student
       @id =   DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def self.find_by_name(name)
+  sql = <<-SQL
+  SELECT *
+  FROM students
+  WHERE name = ?;
+  SQL
+  DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.grade, self.id)
+end
 end
