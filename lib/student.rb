6b3b8621e24e27549d2d7383eb341b63c9461ab5
Chris Harvey <chharvey@icloud.com>
2017-12-18 05:13:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,51 +1,14 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
-  end
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
-    sql = <<-SQL
-    SELECT *
-    FROM students;
-    SQL
-
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?;
-    SQL
-
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-  def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
@@ -60,6 +23,26 @@ class Student
   end
 
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
+    sql = <<-SQL
+    DROP TABLE IF EXISTS students;
+    SQL
     DB[:conn].execute(sql)
   end
+
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students") [0][0]
+  end
+end
